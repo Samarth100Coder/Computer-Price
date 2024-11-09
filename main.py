a286813a87a43e1ea279327e3ceb70af2cfafa16
Samarth100Coder <kumarsamarth272@gmail.com>
2024-11-09 15:31:21 +0530

    commit

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ class comp:
     def __init__(self):
         self.__maxprice=900
     def sell(self):
-        print('Selling Price: {}',format(self.__maxprice))
+        print('Selling Price: {}'.format(self.__maxprice))
     def setMaxPrice(self,price):
         self.__maxprice=price
 c=comp()
